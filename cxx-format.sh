081b5358e04ac722d370ffcfdf6a9ec9b07dba51
Zhi Wang <oukore@gmail.com>
2019-01-28 07:55:24 -0600

    default format

--- cxx-format.sh
+++ cxx-format.sh
@@ -1,94 +1,13 @@
 #!/bin/bash
 set -e
 
-_config1="{ \
-BasedOnStyle: WebKit, \
-ColumnLimit: 80, \
-Language: Cpp, \
-Standard: Cpp11, \
-TabWidth: 8, \
-UseTab: Never, \
-\
-AccessModifierOffset: -3, \
-AlignAfterOpenBracket: DontAlign, \
-AlignConsecutiveAssignments: false, \
-AlignConsecutiveDeclarations: false, \
-AlignEscapedNewlines: Left, \
-AlignOperands: false, \
-AlignTrailingComments: true, \
-\
-AllowAllParametersOfDeclarationOnNextLine: false, \
-BinPackArguments: true, \
-BinPackParameters: true, \
-\
-AllowShortBlocksOnASingleLine: true, \
-AllowShortCaseLabelsOnASingleLine: false, \
-AllowShortFunctionsOnASingleLine: Inline, \
-AllowShortIfStatementsOnASingleLine: false, \
-AllowShortLoopsOnASingleLine: false, \
-\
-AlwaysBreakAfterReturnType: None, \
-AlwaysBreakBeforeMultilineStrings: true, \
-AlwaysBreakTemplateDeclarations: true, \
-\
-BreakBeforeBraces: Custom, \
-BraceWrapping: { \
-  AfterClass: false, \
-  AfterControlStatement: false, \
-  AfterEnum: false, \
-  AfterFunction: false, \
-  AfterNamespace: false, \
-  AfterStruct: false, \
-  AfterUnion: false, \
-  AfterExternBlock: false, \
-  BeforeCatch: false, \
-  BeforeElse: false, \
-  IndentBraces: false, \
-  SplitEmptyFunction: false, \
-  SplitEmptyRecord: false, \
-  SplitEmptyNamespace: false \
-}, \
-BreakBeforeBinaryOperators: All, \
-BreakBeforeInheritanceComma: true, \
-BreakBeforeTernaryOperators: true, \
-BreakConstructorInitializers: BeforeComma, \
-BreakStringLiterals: true, \
-\
-CompactNamespaces: false, \
-ConstructorInitializerAllOnOneLineOrOnePerLine: false, \
-ConstructorInitializerIndentWidth: 3, \
-ContinuationIndentWidth: 3, \
-Cpp11BracedListStyle: true, \
-\
-DerivePointerAlignment: false, \
-FixNamespaceComments: true, \
-IncludeBlocks: Preserve, \
-\
-IndentCaseLabels: false, \
-IndentPPDirectives: AfterHash, \
-IndentWidth: 3, \
-IndentWrappedFunctionNames: false, \
-KeepEmptyLinesAtTheStartOfBlocks: false, \
-MaxEmptyLinesToKeep: 1, \
-NamespaceIndentation: None, \
-\
-PointerAlignment: Left, \
-ReflowComments: true, \
-SortIncludes: true, \
-SortUsingDeclarations: true, \
-\
-SpaceAfterCStyleCast: false, \
-SpaceAfterTemplateKeyword: false, \
-SpaceBeforeAssignmentOperators: true, \
-SpaceBeforeParens: ControlStatements, \
-SpaceInEmptyParentheses: false, \
-SpacesBeforeTrailingComments: 1, \
-SpacesInAngles: false, \
-SpacesInCStyleCastParentheses: false, \
-SpacesInContainerLiterals: false, \
-SpacesInParentheses: false, \
-SpacesInSquareBrackets: false \
-}"
+_config1="                                        {\
+BasedOnStyle:                 LLVM                ,\
+AlignOperands:                false               ,\
+PointerAlignment:             Left                ,\
+IndentPPDirectives:           AfterHash           ,\
+ColumnLimit:                  80                  ,\
+FixNamespaceComments:         false               }"
 
 _dir_of_sh_from_within=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 _prog=$(bash $_dir_of_sh_from_within/_find-clang-format.sh)
