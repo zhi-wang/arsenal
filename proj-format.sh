0bd27919e6a2492c2b7be2c0faf9c5c1a1bf1364
Zhi Wang <oukore@gmail.com>
2019-05-03 16:00:14 -0500

    Fix #pragma problem in clang-format

--- proj-format.sh
+++ proj-format.sh
@@ -4,4 +4,13 @@ set -e
 _dir_of_sh_from_within=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 _prog=$(bash $_dir_of_sh_from_within/_find-clang-format.sh)
 
-$_prog -i -style=file $@
+for x in $@; do
+	# Replace "#pragma " by "//#pragma "
+	sed -i 's/#pragma /\/\/#pragma /g' $x
+
+	# Do format
+	$_prog -i -style=file $x
+
+	# Replace "// *#pragma " by "#pragma
+	sed -i 's/\/\/ *#pragma /#pragma /g' $x
+done
