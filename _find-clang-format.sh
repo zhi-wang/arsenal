6a31e0c226d3da50fd4a576683e9ea9324530a32
Zhi Wang <oukore@gmail.com>
2020-06-08 14:15:27 -0500

    update col-avg and find-clang-format

--- _find-clang-format.sh
+++ _find-clang-format.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-if [ ! -z $(which clang-format-10.0) ]; then
-  _prog=$(which clang-format-10.0)
+if [ ! -z $(which clang-format-10) ]; then
+  _prog=$(which clang-format-10)
 elif [ ! -z $(which clang-format) ]; then
   _prog=$(which clang-format)
 else
@@ -11,13 +11,12 @@ else
 fi
 
 
-# clang-format version 6.0.0 (tags/RELEASE_600/final)
-# -> 6.0.0
-# -> 6
-_version=$($_prog -version | awk '{print $3}')
-_version="${_version%%.*}"
+# Ubuntu clang-format version 10.0.1-++20200529023659+a634a80615b-1~exp1~20200529004255.167
+# -> Ubuntu clang-format version 10 -> 10
+_version=$($_prog -version)
+_version=$(echo "${_version%%.*}" | awk 'NF{ print $NF }')
 if [ $_version -lt 10 ]; then
-  echo Must use clang-format version 10.0.0+
+  echo Must use clang-format version 10+
   exit 1
 fi
 
